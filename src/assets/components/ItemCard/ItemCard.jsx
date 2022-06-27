f1fce6c4e4eabcbd32cbc689de01c2c91f694f59
lerasmrnv <lera.smirnova.flok@gmail.com>
2022-06-27 16:31:07 +0300

    add count studied card and focus button

--- src/assets/components/ItemCard/ItemCard.jsx
+++ src/assets/components/ItemCard/ItemCard.jsx
@@ -1,29 +1,27 @@
-import React, { useState } from 'react';
+import React, { forwardRef, useState } from 'react';
 
-export default function ItemCard(props) {
+function ItemCard(props, ref) {
 
     const [check, setCheck] = useState(false);
 
-
     const handleOnCheck = () => {
         setCheck(!check);
-
+        props.addToStudiedCard();
     }
 
-
-
     return (
-        <div className='item_card'>
+        <div div className='item_card' >
             <h2 className='english-word_item_card'>{props.english}</h2>
             <p className='transcription_item_card'>[{props.transcription}]</p>
             {
                 (check ?
                     <p className='russian-word_item_card'>{props.russian}</p>
-                    : <button className='onCheck_translation' onClick={handleOnCheck}>Проверить</button>
-                )
-            }
+                    : <button className='onCheck_translation' onClick={handleOnCheck} ref={ref} >Проверить</button>
+                )}
         </div>
     )
 }
+
+export default forwardRef(ItemCard);
 
 
--- src/assets/components/SimpleSlider/SimpleSlider.jsx
+++ src/assets/components/SimpleSlider/SimpleSlider.jsx
@@ -1,11 +1,22 @@
 import React from "react";
-import Slider from "react-slick";
-import "./simpleSlider.css";
+import { useState, useEffect, useRef } from "react";
 
+import Slider from "react-slick";
 import ItemCard from "../ItemCard/ItemCard";
 import words from "../words";
 
+
 export default function SimpleSlider() {
+    const [countItems, setCountItems] = useState(0);
+
+    const addToStudiedCard = () => {
+        setCountItems(countItems => countItems + 1);
+    }
+
+    const ref = useRef(null);
+
+    useEffect(() => ref.current.focus(), []);
+
     let settings = {
         infinite: true,
         speed: 500,
@@ -16,10 +27,11 @@ export default function SimpleSlider() {
         <div className="containerSlider">
             <Slider {...settings} >
                 {words.map((word) =>
-                    <ItemCard key={word.id.toString()} english={word.english} russian={word.russian} transcription={word.transcription} />
+                    <ItemCard key={word.id.toString()} english={word.english} russian={word.russian} transcription={word.transcription} addToStudiedCard={addToStudiedCard} />
                 )
                 }
             </Slider>
+            <p className="containerSlider__studiedCard">Изучено слов: {countItems}</p>
         </div>
     );
 }
